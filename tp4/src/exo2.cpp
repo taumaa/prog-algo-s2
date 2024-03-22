b559fb125d9f0762d530e6469a1cc8bf7b6d16a9
taumaa <70432540+taumaa@users.noreply.github.com>
2024-03-22 09:12:03 +0100

    a

---
+++ tp4/src/exo2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <algorithm>
+#include <string>
+#include <numeric>
+
+auto const is_space = [](char letter){ return letter == ' '; };
+
+std::vector<std::string> split_string(std::string const& str) {
+    std::vector<std::string> words;
+
+}
+
+int main() {
+
+
+
+    return 0;
+}
